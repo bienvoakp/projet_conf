595fce41f27cac0db57183f04b4c3dee96a6e891
edouardo <edouardoaf2@gmail.com>
2024-05-07 22:48:02 +0100

    responsivite et fornulaire pour inscription

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-// // Structure de données des conférences
-// const conferences = [
-//     {
-//         date: new Date('2024-03-15'),
-//         subject: "Titre de la première conférence",
-//         location: "Lieu de la première conférence"
-//     },
-//     {
-//         date: new Date('2024-06-20'),
-//         subject: "Titre de la deuxième conférence",
-//         location: "Lieu de la deuxième conférence"
-//     },
-//     // Ajouter d'autres conférences ici...
-// ];
-
 // Fonction pour trier les conférences par date
 function sortConferencesByDate(conferences) {
     return conferences.sort((a, b) => a.date - b.date);
@@ -23,76 +8,116 @@ function displayConferenceInfo(conference, list) {
     /* Pour chaque conference */
     const card = document.createElement('div');
     card.classList.add('conference');
+    card.classList.add('col-8'); //class pour system grid
+    card.classList.add('col-lg-4'); //class pour grid
 
+    //contenu
     card.innerHTML = `
     <div class="conference-image">
         <img src="${conference.urlImage}" alt="Conférence">
     </div>
     <div class="conference-info">
-        <h3>${conference.details}</h3>
+        <h3 class="text-success">${conference.details}</h3>
         <div class="details">
             <h4>${conference.subject}</h4>
             <p>Date : ${conference.date}</p>
             <p>Lieu : ${conference.location}</p>
             <p>Sujet : Sujet de la conférence</p>
             <p>Heure : ${conference.time} - ${conference.endTime}</p>
-            <p>${conference.status === "upcoming" ? '<button class="badge btn-outline-success">S\'inscrire</button>' : ''}</p>
+            <p>${conference.status === "upcoming" ? `<button nom="${conference.id}" class="badge  btn btn-success px-4 py-2 display-2">S\'inscrire</button>` : ''}</p>
         </div>
     </div>
     `;
     list.appendChild(card);
-    // const listItem = document.createElement("li");
-    // listItem.innerHTML = `
-    //     <strong>${conference.subject}</strong><br>
-    //     Date : ${conference.date}<br>
-    //     Lieu : ${conference.location}<br>
-    //     Heure : ${conference.time}<br>
-    //     Détails : ${conference.details}<br>
-    //     ${conference.status === "upcoming" ? '<span class="badge badge-primary">À venir</span>' : ''}
-    // `;
-    // list.appendChild(listItem);
 }
 
 // Chargement de la liste des conférences
-fetch('js/conferences.json')
+fetch('./js/conferences.json')
 .then(response => response.json())
 .then(data => {
     // Tu peux utiliser les données ici
     console.log(data);
 
+    //Initialisation
     let conferences = data;
     const currentDate = new Date();
 
     // Filtrer les conférences passées et à venir
     const pastConferences = conferences.filter( (conference) => {
         //Ramener les dates au meme fornat que currentDate
-        let formatedDate = new Date(conference.date +'T'+conference.time);
-
-        console.log(formatedDate);        
+        let formatedDate = new Date(conference.date +'T'+conference.time);    
         return formatedDate < currentDate;
     }
     );
     const upcomingConferences = conferences.filter((conference) => {
         //Ramener les dates au meme fornat que currentDate
         let formatedDate = new Date(conference.date +'T'+conference.time);
-
         return formatedDate >= currentDate;
     }
     );
+
+    // Affichage des conférences à venir
+    const upcomingConferencesCard = document.querySelector('#upcoming-conferences .row');
+    upcomingConferences.forEach(conference => {
+        displayConferenceInfo(conference, upcomingConferencesCard);
+    });
     
     // Affichage des conférences passées
-    const pastConferencesCard = document.querySelector('#past-conferences .container');
+    const pastConferencesCard = document.querySelector('#past-conferences .row');
     pastConferences.forEach(conference => {
         displayConferenceInfo(conference, pastConferencesCard);
     });
     
-    // Affichage des conférences à venir
-    const upcomingConferencesCard = document.querySelector('#upcoming-conferences .container');
-    upcomingConferences.forEach(conference => {
-        displayConferenceInfo(conference, upcomingConferencesCard);
-    });
+    //Info supplementaires au formulaire
+    const form = document.querySelector('.registration-form');
+    const referenceElement = form.children[1];
+    console.log(referenceElement);
+
+    const btnInscrires = document.querySelectorAll('.badge');
+    //dialog
+    const dialog = document.querySelector('dialog');
+
+    btnInscrires.forEach(btn => {
+            btn.onclick= (e)=>{
+                //id de la conférence selectionée
+                const idElemt = e.srcElement.attributes[0].value;
+
+                //creer un input additionnel aux formulaire contenant le titre de la conference choisi
+                const titreConference = document.createElement('div');
+                titreConference.classList.add('row');
+                titreConference.innerHTML= `
+                <fieldset disabled="disabled">
+                    <div class="form-group">
+                        <label for="titre">Titre :</label>
+                        <textarea class="form-control" id="titre" name="titre">${conferences[idElemt].subject}</textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Description :</label>
+                        <textarea class="form-control" id="description" name="description">${conferences[idElemt].details}</textarea>
+                    </div>
+                </fieldset>
+                    `
+                form.insertBefore( titreConference, referenceElement);
+
+                //dialog opening
+                dialog.showModal();
+            }
+                
+        });
+        
+        //submit
+        document.getElementById('inscrire').onclick = ()=>{
+            dialog.close();
+        }
+
+        //fermer formulaire
+        document.querySelector('del').onclick = ()=>{
+            dialog.close();
+        }
+
     })
 
     .catch(error => {
         console.error('Erreur lors du chargement des données JSON :', error);
     });
+
