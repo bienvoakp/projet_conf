328c973687d8bfe3e466718c4fad49da5c38d8f4
bienvoakp <bienvenuakpo@gmail.com>
2024-05-17 22:10:25 +0200

    on peut désormais se recevoir les données du formulaire dans la base de données

--- conferences.php
+++ conferences.php
@@ -1,5 +1,5 @@
 <?php
-    include_once("db_config.php");
+   // include_once("db_config.php");
 
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
@@ -12,7 +12,50 @@
         $tel = htmlspecialchars($_POST['tel']);
         $email = htmlspecialchars($_POST['email']);
 
-        try {
+        echo 'Bonjour ' . $nom . ' ' . $prenom . ' ! Votre inscription a été enregistrée avec succès. Merci ! <br><br>';
+
+    }
+
+    //CONNEXION A LA BASE DE DONNEES
+
+    try{
+        $bdd= new PDO('mysql:host=localhost; dbname=countrycall;charset = utf-8','root','');
+
+        /*echo "Connexion réussie à la base de données !";*/
+
+    } catch(Exception $e) {
+        die('Erreur  :'. $e->getMessage());
+
+    }
+
+    //On veut ajouter un nouvel utilisateur qui remplit le formulaire à notre base de données
+
+    if(isset($_POST['nom']) && isset( $_POST['prenom']) && isset($_POST['tel']) && isset($_POST['email'])) {
+        $nom=$_POST['nom'];
+        $prenom=$_POST['prenom'];
+        $tel=$_POST['tel'];
+        $email=$_POST['email'];
+    }
+
+    $requete=$bdd->prepare('INSERT INTO inscription_conferences(nom, prenom, contacts, email) VALUES (?, ?, ?, ?)') 
+    or die(print_r($bdd->errorInfo()));
+    $requete->execute(array($nom, $prenom, $tel, $email));
+    
+    
+    
+    
+    //On peut lire les données contenues dans notre base grâce à une requête php
+
+    $requete= $bdd->query('SELECT * FROM inscription_conferences') ;
+
+
+       /* while($donnees=$requete->fetch()){
+
+           echo '<br><br>' ;
+            echo $donnees ['nom'];
+        }*/
+
+        /*try {
             $con = mysqli_connect($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_DATABASE );
         } catch (\Throwable $th) {
             throw $th;
@@ -39,7 +82,25 @@
         // Close the statement and connection
         mysqli_stmt_close($stmt);
         mysqli_close($conn);
+
+
+//Avec PDO, on aurait pu également procéder comme suit.
+
+
+        $dsn = 'mysql:host=localhost;dbname=votre_base_de_donnees;charset=utf8';
+        $username = 'votre_nom_utilisateur';
+        $password = 'votre_mot_de_passe';
+
+        try {
+            $pdo = new PDO($dsn, $username, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connexion réussie!";
+        } catch (PDOException $e) {
+            echo "Erreur de connexion : " . $e->getMessage();
+        }
+
+
         
-    }
+    }*/
 
 ?>
